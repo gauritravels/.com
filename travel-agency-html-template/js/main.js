9042641049f7f5383f8d35af1850b4654aa07f38
gauritravels <140881281+gauritravels@users.noreply.github.com>
2023-10-05 23:11:03 -0700

    Add files via upload

---
+++ travel-agency-html-template/js/main.js
@@ -0,0 +1,141 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+    
+    
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $(window).on("load resize", function() {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+            function() {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function() {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
+// Get the form element
+const form = document.querySelector('form');
+
+// Add event listener for form submission
+form.addEventListener('submit', function(e) {
+  e.preventDefault(); // Prevent form from being submitted
+
+  // Get the form input values
+  const name = document.querySelector('#name').value;
+  const email = document.querySelector('#email').value;
+  const datetime = document.querySelector('#datetime').value;
+  const destination = document.querySelector('#destination').value;
+  const message = document.querySelector('#message').value;
+
+  // Validate the form inputs
+  if (name === '' || email === '' || datetime === '' || destination === '') {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  // Create object with the form data
+  const formData = {
+    name,
+    email,
+    datetime,
+    destination,
+    message
+  };
+
+  // You can now send this form data to your server using AJAX or submit the form normally
+  // For example, using fetch API to send the form data via POST request
+  fetch('/your-server-endpoint', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); // You can do something with the response from the server
+    form.reset(); // Reset the form after successful submission
+  })
+  .catch(error => {
+    console.error('Error:', error); // Handle any errors that occur during the request
+  });
+});
+
